2e77904320b1a9a75e0c49db9845c5557fc8001e
Balla Kavya <ballakavya594@gmail.com>
2024-08-10 10:42:45 +0530

    "Update app routing: change default route redirect and path for HomeComponent"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,8 +8,8 @@ import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: '/', component: HomeComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'skills', component: SkillsComponent },
   { path: 'projects', component: ProjectsComponent },
