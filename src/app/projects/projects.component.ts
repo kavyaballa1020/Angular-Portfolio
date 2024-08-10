35f86bcf8c3d19e71444d5ade255d892ef2599ba
Balla Kavya <ballakavya594@gmail.com>
2024-08-10 08:56:28 +0530

    "Updated project 2 description and title in projects component."

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,7 +9,7 @@ export class ProjectsComponent {
   projects = [
     { title: 'Project 1', description: 'A web application built with Angular.', imageUrl: '/blog.png', liveDemoLink: 'https://example.com/demo1', gitRepoLink: 'https://github.com/user/repo1' },
     { title: 'Project 3', description: 'A mobile app developed with React Native.', imageUrl: '/Resume.png', liveDemoLink: 'https://example.com/demo3', gitRepoLink: 'https://github.com/user/repo3' },
-    { title: 'Project 2', description: 'A backend API developed using Node.js.', imageUrl: '/Hexa.png', liveDemoLink: 'https://hexagenius-calculator.onrender.com/', gitRepoLink: 'https://github.com/user/repo2' },
+    { title: 'HexaGenius Calculator', description: 'This is a user-friendly Number System Calculator web application. It allows you to effortlessly convert numbers between different number systems, including Decimal, Binary, Octal, and Hexadecimal.', imageUrl: '/Hexa.png', liveDemoLink: 'https://hexagenius-calculator.onrender.com/', gitRepoLink: 'https://github.com/user/repo2' },
     { title: 'Project 4', description: 'A machine learning model for image classification.', imageUrl: '/Markdown.png', liveDemoLink: 'https://kav-markdown-editor.vercel.app/', gitRepoLink: 'https://github.com/user/repo4' },
     { title: 'Project 5', description: 'An e-commerce website using Django.', imageUrl: '/SmartPond.png', liveDemoLink: 'https://example.com/demo5', gitRepoLink: 'https://github.com/user/repo5' },
     { title: 'Project 6', description: 'A real-time chat application using WebSocket.', imageUrl: '/image.png', liveDemoLink: 'https://example.com/demo6', gitRepoLink: 'https://github.com/user/repo6' }
