b35bb547fe6e8b33438d069d3b688fb1d54df43d
Balla Kavya <ballakavya594@gmail.com>
2024-08-10 11:30:41 +0530

    "Added Router import and navigateToProjects method to AboutComponent"

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  constructor(private router: Router) {}
 
+  navigateToProjects() {
+    this.router.navigate(['/projects']);
+  }
 }
