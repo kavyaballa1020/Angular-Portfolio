ad6decf35c2cd7322691fc472557fc2bcc3002c2
Balla Kavya <ballakavya594@gmail.com>
2024-08-14 10:55:10 +0530

    "Added isNavOpen property and toggleNav method to HeaderComponent"

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isNavOpen = false;
+
+  toggleNav() {
+    this.isNavOpen = !this.isNavOpen;
+  }
 }
